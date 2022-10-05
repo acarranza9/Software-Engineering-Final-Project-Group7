99894ec9acf64d250dc16993ac2825db6f8f6f61
acarranza9 <torresadrian059@gmail.com>
2022-10-05 10:18:44 -0500

    push github pages
    github pages setup

---
+++ src/user/User.java
@@ -0,0 +1,41 @@
+package user;
+
+public class user{
+
+    private int id;
+    private String username;
+    private String password;
+
+    public user(int id, String username, String password){
+        this.id;
+        this.username = username;
+        this.password = password;
+    }
+    public int getId(){
+        return id;
+
+    }
+    public int setId(){
+        this.id =  id;
+
+    }
+
+    public int getUsername(){
+        return username;
+
+    }
+    public int setUsername(){
+        this.username =  username;
+
+    }
+
+    public int getPassword(){
+        return password;
+
+    }
+    public int setPassword(){
+        this.password =  password;
+
+    }
+
+}
